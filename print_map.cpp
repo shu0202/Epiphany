c95dddeb70eea2c9358b6a67edecba93fcaef277
shu0202 <91327331+shu0202@users.noreply.github.com>
2021-11-21 17:31:03 +0800

    Update print_map.cpp

--- print_map.cpp
+++ print_map.cpp
@@ -8,8 +8,8 @@ using namespace std;
 
 void print_map(int map_no, int xpos, int ypos){
     clear();
-    for (int i=0; i<10; i++){
-        for (int j=0; j<10; j++){
+    for (int i=0; i<30; i++){
+        for (int j=0; j<30; j++){
             if (i == ypos and j == xpos){
                 printw("* ");
             }
