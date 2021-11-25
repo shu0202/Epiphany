e9d60ecbe451b8417752613f0e8de7c4a8e94378
shu0202 <91327331+shu0202@users.noreply.github.com>
2021-11-25 23:45:04 +0800

    Update map.cpp

--- map.cpp
+++ map.cpp
@@ -8,11 +8,13 @@ void make_prop(char map[30][30]){
     for (int i=15; i<30; i++){
         map[0][i] = '.';
     }
-    for (int i=0; i<14; i++){
+    for (int i=0; i<15; i++){
         map[1][i] = '^';
     }
-    map [1][14] = '~';
-    //for (int i=0; )
+    map [1][15] = '~';
+    for (int i=16; i<30;i++){
+        map[1][i] == '.';
+    }
 
     return;
 }
