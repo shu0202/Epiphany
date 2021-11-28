62e5cb11fe588f6a92c3c72e9fd722f6c8aaf0ea
bohrisc <ccyboris@gmail.com>
2021-11-28 22:51:40 +0800

    function related to in-game

---
+++ inbattle.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include "monster.h"
+#include "newgame.h"
+using namespace std;
+
+int inbattle(player_stat *player, monster *mon){
+    int k, battlechoice, ch;
+    double critchance, misschance;
+    k = 0;
+    srand(time(NULL));
+    while (true){
+        clear();
+        misschance = ((double) rand()/RAND_MAX);
+        critchance = ((double) rand()/RAND_MAX);
+        if (k%2 == 0){
+            //battle menu
+            battlechoice = 0;
+            print_battle_menu(battlechoice, player, mon);
+            ch = getch();  
+            while (ch != 13){
+                clear();
+                if (ch == 119){
+                    battlechoice -= 1;
+                    if (battlechoice < 0){
+                        battlechoice = 1;
+                    }
+                    print_battle_menu(battlechoice, player, mon);
+                }
+                else if (ch == 115){
+                    battlechoice += 1;
+                    if (battlechoice > 1){
+                        battlechoice = 0;
+                    }
+                    print_battle_menu(battlechoice, player, mon);
+                }
+                else
+                    print_battle_menu(battlechoice, player, mon);
+                ch = getch();
+            }
+        }
+        clear();
+        //player turn
+        if (k % 2 == 0){
+            //player chose attack
+            if (battlechoice == 0){
+                if (misschance <= 0.1){
+                    printw("Attack missed\n");
+                    k += 1;
+                    printw("(Press any key to proceed to next turn...)\n");
+                    getch();
+                    continue;
+                }
+                else{
+                    if (critchance <= 0.1){
+                        printw("It's a critical hit!\n");
+                        printw("%s got hit by %d\n", mon->name, (player->atk)*2);
+                        mon->hp -= (player->atk)*2;
+                    }
+                    else{
+                        printw("%s got hit by %d\n", mon->name, player->atk);
+                        mon->hp -= player->atk;
+                    }
+                }
+            }
+            //player chose talk
+            else if (battlechoice == 1){
+                printw("%s: %s\n", mon->name, mon->talk);
+                printw("You spared %s...\n", mon->name);
+                printw("(Press any key to leave...)\n");
+                getch();
+                return 2;
+            }
+        }
+        //monster turn
+        else{
+            clear();
+            printw("Opponent's turn:\n");
+            if (misschance <= 0.1){
+                printw("Attack missed\n");
+                k += 1;
+                printw("(Press any key to proceed to next turn...)\n");
+                getch();
+                continue;
+            }
+            else{
+                if (critchance <= 0.1){
+                        printw("It's a critical hit!\n");
+                        printw("%s got hit by %d\n", player->name, (mon->atk)*2);
+                        player->hp -= (mon->atk)*2;
+                    }
+                    else{
+                        printw("%s got hit by %d\n", player->name, mon->atk);
+                        player->hp -= mon->atk;
+                    }
+            }
+        }
+        if (player->hp <= 0){
+            printw("You fainted...\n");
+            printw("(Press any key to respawn...)\n");
+            getch();
+            return 0;
+        }
+        if (mon->hp <= 0){
+            printw("%s: %s\n", mon->name, mon->dead);
+            printw("You killed %s...\n", mon->name);
+            printw("(Press any key to leave...)\n");
+            getch();
+            return 1;
+        }
+        k += 1;
+        printw("(Press any key to proceed to next turn...)\n");
+        getch();  
+    }
+}
---
+++ print_battle_menu.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "monster.h"
+#include "newgame.h"
+using namespace std;
+
+void print_battle_menu(int battlechoice, player_stat *player, monster *mon){
+    if (battlechoice == 0){
+        printw(" Your turn:\n");
+        printw(" ___  ___  ___  ___  ___  ___  ___  ___ \n");
+        printw(" %s Hp: %3d\n", mon->name, mon->hp);
+        printw(" %s Hp: %3d\n", player->name, player->hp);
+        printw(" Attack<--\n");
+        printw(" Talk\n");
+        printw(" ___  ___  ___  ___  ___  ___  ___  ___ \n");
+    }
+    else if (battlechoice == 1){
+        printw(" Your turn:\n");
+        printw(" ___  ___  ___  ___  ___  ___  ___  ___ \n");
+        printw(" %s Hp: %3d\n", mon->name, mon->hp);
+        printw(" %s Hp: %3d\n", player->name, player->hp);
+        printw(" Attack\n");
+        printw(" Talk<--\n");
+        printw(" ___  ___  ___  ___  ___  ___  ___  ___ \n");
+    }
+}
