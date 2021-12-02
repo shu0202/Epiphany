f9fe3008d7b706abca6f5e7b32d89646264116bc
Boris Chang <90781035+bohrisc@users.noreply.github.com>
2021-12-02 16:00:32 +0800

    Update newgame.cpp

--- newgame.cpp
+++ newgame.cpp
@@ -106,7 +106,7 @@ void newgame(int gstatus){
     lester->no = 11;
     lester->hp = 300;
     lester->atk = 100;
-    strcpy(lester->dead, "You kill one me, there's still a thousand of me");
+    strcpy(lester->dead, "ughhh...");
     strcpy(lester->talk, "Men communicate with fists and sweats");
 
     monster* flower = new monster;
